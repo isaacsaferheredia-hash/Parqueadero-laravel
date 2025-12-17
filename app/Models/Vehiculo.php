76ff9daaa37c638760471929dded04a9b9620942
isaacsaferheredia-hash <isaacsaferheredia@gmail.com>
2025-12-16 23:20:28 -0500

    CRUD Parqueadero completo

---
+++ app/Models/Vehiculo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+    protected $table = 'vehiculos';
+    protected $fillable = [
+        'placa',
+        'tipo',
+        'propietario',
+        'observaciones',
+    ];
+    static public function getVehiculos(){
+        return self::all();
+    }
+    static public function getVehiculosbyId($id){
+        return self :: find($id);
+    }
+    public static function createVehiculo(array $data)
+    {
+        return self::create($data);
+    }
+
+    static public function updateVehiculo($vehiculo, array $data)
+    {
+        return $vehiculo->update($data);
+    }
+
+    static public function deleteVehiculo(Vehiculo $vehiculo){
+        $vehiculo -> delete();
+    }
+}
